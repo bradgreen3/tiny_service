577ccee6f39f9062e7661d2b4c93a97e8907daee
Brad <bgreen3@uoregon.edu>
2017-02-13 11:21:23 -0700

    Experiment

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -2,7 +2,7 @@ class CommitsController < ApplicationController
   # skip_before_action :verify_authenticty_token
 
   def create
-    @commit = Commit.create(commit_id: params["commits"][0]["id"][0..6], commit_message: params["commits"][0]["message"], author: params["commits"][0]["author"]["name"], timestamp: params["commits"][0]["timestamp"])
+    @commit = Commit.create(commit_id: params["payload"]["commits"][0]["id"][0..6], commit_message: params["payload"]["commits"][0]["message"], author: params["payload"]["commits"][0]["author"]["name"], timestamp: params["payload"]["commits"][0]["timestamp"])
     render json: @commit.to_json, status: 200
   end
 
